752b80d64b1c300dd0de699c4368b21ac8b7da08
Mohammad baig <mirza.mohammad19@hotmail.com>
2021-07-23 00:18:04 +0400

    Add helmet

--- src/server.js
+++ src/server.js
@@ -28,6 +28,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan);
+app.use(helmet());
 app.listen(PORT, () => {
   logger.info(`Server is running at PORT:${PORT}`);
 });
